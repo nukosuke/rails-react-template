db0f13b6d65a44a1112fc4f7884005132c80c3ba
nukosuke <39.nuko@gmail.com>
2017-02-06 07:23:39 +0900

    add react render methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,38 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def home
+    render_for_react()
+  end
+
+  private
+
+  # ref: http://r7kamura.hatenablog.com/entry/2016/10/10/173610
+  def common_props
+    {
+      currentUser: current_user,
+    }
+  end
+
+  def render_for_react(props: {}, status: 200)
+    if request.format.json?
+      response.headers["Cache-Control"] = "no-cache, no-store"
+      response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+      response.headers["Pragma"] = "no-cache"
+      render(
+        json: common_props.merge(props),
+        status: status,
+      )
+    else
+      render(
+        html: view_context.react_component(
+          "Router",
+          prerender: true,
+          props: common_props.merge(props).as_json,
+        ),
+        layout: true,
+        status: status,
+      )
+    end
+  end
 end
