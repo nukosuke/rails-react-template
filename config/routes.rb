7db9444373da0b358c1d84e193e1e83b268519b3
nukosuke <nukosuke@users.noreply.github.com>
2017-02-06 10:01:35 +0900

    #2 Feature/devise (#6)
    * add devise gem
    
    * bundle exec rails g devise:install
    
    * bundle exec rails g devise User
    
    * config devise User migrate
    
    * install devise views & controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   root to: 'application#home'
+
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    #unlocks: 'users/unlocks',
+    #omniauth_callbacks: 'users/omniauth_callbacks',
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
